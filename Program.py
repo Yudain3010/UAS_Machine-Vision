d2b4c9ddc0e88ec329f8da6dcd216116a0e80ea0
Yudain3010 <143328940+Yudain3010@users.noreply.github.com>
2024-01-12 22:03:17 +0700

    Update Program.py

--- Program.py
+++ Program.py
@@ -10,15 +10,17 @@ images, labels = loadlocal_mnist(
     labels_path='images/mnist-dataset/train-labels-idx1-ubyte'
 )
 
-hog_features = [
-    hog(image.reshape((28, 28)), orientations=8, pixels_per_cell=(4, 4), cells_per_block=(1, 1), visualize=True)[0]
-    for image in images[:100]
-]
-
+num_images_to_process = 200
+hog_features = []
+for i in range(num_images_to_process):
+    image = images[i]
+    fd, hog_image = hog(image.reshape((28, 28)), orientations=8, pixels_per_cell=(4, 4), 
+	           cells_per_block=(1, 1), visualize=True)
+    hog_features.append(fd)
 
 hog_features = np.array(hog_features)
 
-X_train, X_test, y_train, y_test = train_test_split(hog_features, labels[:100], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(hog_features, labels[:num_images_to_process], test_size=0.2, random_state=42)
 
 svm_classifier = SVC(kernel='linear')
 svm_classifier.fit(X_train, y_train)
@@ -26,8 +28,10 @@ print(hog_features, 'hog features')
 
 y_pred = svm_classifier.predict(X_test)
 
-akurasi = accuracy_score(y_test, y_pred)
-presisi = precision_score(y_test, y_pred, average='weighted')
-confussio_matrix = confusion_matrix(y_test, y_pred)
-print(akurasi)
-print(confussio_matrix)
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted', zero_division=1)
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print("Accuracy: {:.2f}%".format(accuracy * 100))
+print("Precision: {:2f}%".format(precision * 100))
+print("Confusion Matrix:\n", conf_matrix)
